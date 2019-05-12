1f691e5d1eae31a7fb15283653c69fcb6db6cc86
Arkadiusz Krauzowicz <arktos.k@gmail.com>
2019-05-13 00:34:42 +0200

    Initial typescript CLI setup

---
+++ packages/shoper-scripts/src/scripts/start.ts
@@ -0,0 +1,3 @@
+
+
+export const start: string = 'Start script';
--- packages/shoper-scripts/src/shoper-scripts.ts
+++ packages/shoper-scripts/src/shoper-scripts.ts
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log("Shoper Scripts");
+console.log('Shoper Scripts');
+
+console.log(process.argv.slice(2));
+
+const enum Scripts {
+    start = 'start',
+    build = 'build',
+    test = 'test',
+}
+
+console.log(Scripts.start);
+
+process.on('unhandledRejection', (error) => { throw error; });
