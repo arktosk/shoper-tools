37413f14c6607094bc33093dff191c5d42477287
Arkadiusz Krauzowicz <arktos.k@gmail.com>
2019-08-08 00:33:11 +0200

    Setup spawning processes by CLI

--- packages/shoper-scripts/src/scripts/start.ts
+++ packages/shoper-scripts/src/scripts/start.ts
@@ -3,7 +3,15 @@
  * 1. Start dev server
  * 2. Set watchers for files
  * 3. Compile JS, LESS, VUE
- * 3. Deploy by WebDav
- * 4. Inject middleware
+ * 4. Deploy by WebDav
+ * 5. Inject middleware
  */
+console.log('1. Start dev server');
+console.log('2. Set watchers for files');
+console.log('3. Compile JS, LESS, VUE');
+console.log('4. Deploy by WebDav');
+console.log('5. Inject middleware');
+
+console.log(process.cwd());
+
 export const start = "Start script 123";
--- packages/shoper-scripts/src/shoper-scripts.ts
+++ packages/shoper-scripts/src/shoper-scripts.ts
@@ -1,13 +1,48 @@
 #!/usr/bin/env node
 
-import './config/env';
+import * as spawn from 'cross-spawn';
+import * as cp from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
 
-import {paths} from './config';
-// console.log(process.env.WEBDAV_HOSTNAME);
+/** Register available scripts */
+export const shoperScripts: string[] = [
+  'start',
+];
 
-process.on(
-  "unhandledRejection",
-  (error: Error): void => {
+/**
+ * Makes the script crash on unhandled rejections instead of silently
+ * ignoring them. In the future, promise rejections that are not handled will
+ * terminate the Node.js process with a non-zero exit code.
+ */
+process.on("unhandledRejection", (error: Error): void => {
     throw error;
   }
 );
+
+const processArguments: string[] = process.argv.slice(2);
+
+const scriptsPath: string = path.resolve(__dirname, './scripts');
+const scriptIndex: number = processArguments.findIndex((argument: string): boolean => shoperScripts.indexOf(argument) !== -1);
+console.log(scriptIndex);
+console.log(scriptsPath);
+// process.chdir('/tmp');
+// console.log(process.cwd());
+
+if (scriptIndex === -1) {
+  throw new Error(
+    `Unknown script: "${processArguments[0]}"`
+  );
+}
+
+const result: cp.SpawnSyncReturns<Buffer> = spawn.sync(
+  'node',
+  processArguments.slice(0, scriptIndex)
+    .concat(require.resolve('./scripts/' + processArguments[scriptIndex]))
+    .concat(processArguments.slice(scriptIndex + 1)),
+  { stdio: 'inherit' }
+);
+
+fs.readdirSync(scriptsPath).filter((file) => file.match(/\.js$/)).forEach((file: string) => {
+  console.log(path.basename(file, '.js'));
+});
